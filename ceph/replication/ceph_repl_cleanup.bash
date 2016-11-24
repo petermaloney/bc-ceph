97ba3ecd43c2b8ed28ccecab9eb481c6bb05a530
Peter Maloney <peter.maloney@brockmann-consult.de>
2016-11-24 11:24:35 +0100

    flock moved outside the loop; made it use ssh only one time; shortened and indented output

--- ceph/replication/ceph_repl_cleanup.bash
+++ ceph/replication/ceph_repl_cleanup.bash
@@ -2,30 +2,53 @@
 #
 # Remove all remote snaps except the latest that is on backup
 
-for image in $(ssh ceph1 rbd ls proxmox); do
-    (
-    if ! flock -n 9; then
-        log_warn "can't get lock... quitting"
-        exit 1
-    fi
-    
+# TODO: make one ssh connection and reuse it
+
+
+echo "reading snap list"
+IFS=$'\n'
+snap_data=($(
+    ssh ceph1 '
+            for image in $(rbd ls proxmox); do
+                for snap in $(rbd snap ls proxmox/"$image" | awk '"'"'NR!=1{print $2}'"'"' | sort -r); do
+                    echo "${image}@${snap}"
+                done
+            done
+        '
+))
+
+list_images(){
+    printf "%s\n" "${snap_data[@]}" | awk -F@ '{print $1}' | uniq
+}
+list_snaps(){
+    local image="$1"
+    printf "%s\n" "${snap_data[@]}" | grep -E "^${image}@" | awk -F@ '{print $2}'
+}
+
+(
+if ! flock -n 9; then
+    log_warn "can't get lock... quitting"
+    exit 1
+fi
+for image in $(list_images); do
+    echo "image = $image"
     remote_local_match=
-    for snap in $(ssh ceph1 rbd snap ls proxmox/"$image" | awk 'NR!=1{print $2}' | sort -r); do
-        echo "remote = ${image}@${snap}"
+    for snap in $(list_snaps "$image" | sort -r); do
+        echo -n "    remote = ${image}@${snap}... "
         if [ -e "/data/ceph-repl/proxmox/${image}/${snap}" ]; then
-            echo "local found"
+            echo " local found"
             remote_local_match="$snap"
             break
         else
-            echo "local not found"
+            echo " local not found"
         fi
     done
     list=()
-    for snap in $(ssh ceph1 rbd snap ls proxmox/"$image" | awk 'NR!=1{print $2}' | sort -r); do
+    for snap in $(list_snaps "$image" | sort -r); do
         if [ "$snap" = "$remote_local_match" ]; then
             continue
         fi
-        echo "removing $snap"
+        echo "    removing $snap"
         list+=("$snap")
     done
     
@@ -38,6 +61,6 @@ for image in $(ssh ceph1 rbd ls proxmox); do
             rbd snap rm "proxmox/${image}@\${x}"
         done
         "
-    ) 9>/var/run/ceph_repl.lock
 done
+) 9>/var/run/ceph_repl.lock
 
